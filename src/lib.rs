f8cd72356f18e094a247f218bc2590e3d374dca8
Marwin <Marwin.ack@googlemail.com>
2022-06-12 14:10:40 +0200

    fix failing test (thanks flo), and write a new one for buzz-case

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,10 @@ pub fn main() {
 }
 
 pub fn fizzbuzz(x: i32) -> String {
+    let is_divisible_by_3 = x % 3 == 0;
+
+    if x != 0 && is_divisible_by_3 {
+        return String::from("fizz");
+    }
     return format!("{x}")
 }
--- tests/main_test.rs
+++ tests/main_test.rs
@@ -7,9 +7,24 @@ fn it_returns_zero_if_input_zero() {
 }
 
 #[test]
+fn it_returns_two_if_input_2() {
+    let actual: String = fizzbuzz(2);
+    assert_eq!(actual, "2");
+}
+
+#[test]
 fn it_returns_fizz_on_divisible_by_three() {
-    for input in (1..3) {
+    for input in 1..3 {
         let actual: String = fizzbuzz(3 * input);
         assert_eq!(actual, "fizz")
     }
 }
+
+#[test]
+fn it_returns_buzz_on_divisible_by_five() {
+    for input in [1,2,4] {
+        let actual: String = fizzbuzz(5 * input);
+        assert_eq!(actual, "buzz")
+    }
+}
+
